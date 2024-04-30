6f3b8ff55e314c1f5a5e8a8978cadb8e844a6e78
PB07-code <pparul1310@yahoo.com>
2024-04-30 15:33:01 +0530

    Added stubs for Junit Testing
    Added stubs for Junit Testing

---
+++ demo-backend/src/test/java/com/example/demo/service/impl/UserServiceImplTest.java
@@ -0,0 +1,38 @@
+package com.example.demo.service.impl;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void getUserById() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+}
